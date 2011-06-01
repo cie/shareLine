936881bead3a76ad62dea008620c3d6fe97f3ee4
Kalló Bernát <kallo.bernat@gmail.com>
2011-06-01 11:48:18 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareLine::Application.config.secret_token = 'feb1c88a37d06f3934b30894f52125b58fddb279f36a6bc04ae51fd7700f1064a4a5328d005adcd35432cad9380a6c351e8046c44df9e151f9b1df7b73b041b7'
