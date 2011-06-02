1c319500eb7ea2197f41106f75fe461c84d92489
Kalló Bernát <kallo.bernat@gmail.com>
2011-06-02 20:16:49 +0200

    restored messed up migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110601103203) do
+ActiveRecord::Schema.define(:version => 20110601101023) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'password' for column 'passwd'
